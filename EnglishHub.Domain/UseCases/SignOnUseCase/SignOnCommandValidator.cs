aeaccb7a0e7101ceadf899869ca3c4b1d84372ba
shombal1 <sasha.ruchits.06@gmail.com>
2024-07-19 13:53:29 +0300

    Added a login constraint when created by the user

--- EnglishHub.Domain/UseCases/SignOnUseCase/SignOnCommandValidator.cs
+++ EnglishHub.Domain/UseCases/SignOnUseCase/SignOnCommandValidator.cs
@@ -7,7 +7,8 @@ public class SignOnCommandValidator : AbstractValidator<SignOnCommand>
     public SignOnCommandValidator()
     {
         RuleFor(s => s.Login)
-            .NotEmpty().WithErrorCode("Empty login");
+            .NotEmpty().WithErrorCode("Empty login")
+            .MaximumLength(30).WithErrorCode("login too long");
 
         RuleFor(s => s.Password)
             .NotEmpty().WithErrorCode("Empty password");
